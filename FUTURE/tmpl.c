d5e998e56de0216a80529994bc5867d0ec282c2a
sebastien peterson boudreau <sebastien.peterson.boudreau@gmail.com>
2024-11-13 22:57:46 -0400

    begin implementing resubstitution

--- FUTURE/tmpl.c
+++ FUTURE/tmpl.c
@@ -2,52 +2,93 @@
 #include <stdio.h>
 #include <string.h>
 
-static void usage(char *argv0)
+#define die(msg) fprintf(stderr, "%s: " msg "\n", argv[0])
+#define USAGE "[ -s char ][ -h char ] TEMPLATE FILE"
+
+static char delim;
+static char special;
+
+static char **mapsecs(FILE *src, unsigned int *mapsize)
 {
-	fprintf(stdout, "%s - [ -s character][ -h character] TEMPLATE FILE\n", argv0);
-	exit(1);
+	int    size   = 10;
+	char **secmap = malloc(sizeof(*secmap) * size);
+
+	char  *line = strdup("#@HEADER\n");
+	size_t llen = 0;
+	unsigned long lno = 0;
+	do {
+		if (lno >= size) {
+			size *= 2;
+			if (!(secmap = realloc(secmap, size * sizeof(*secmap))))
+				return NULL;
+		}
+		secmap[lno++] = (*line == delim) ? strdup(line) : NULL;
+	} while (getline(&line, &llen, src) > 0);
+	free(line);
+	line = NULL;
+
+	*mapsize = size;
+	return secmap;
+}
+
+static void putsec(char *sec, FILE *src, char **secmap, unsigned int size)
+{
+	int target = 0;
+	while (target < size && secmap[target] && strcmp(sec, secmap[target++]));
+
+	size_t llen = 0;
+	char  *line = NULL;
+	unsigned long lno = 0;
+	while (getline(&line, &llen, src) > 0) {
+
+	}
+}
+
+static void subst(FILE *tmpl, FILE *src, char **secmap, unsigned int size)
+{
+	size_t llen = 0;
+	char *line = NULL;
+	while (getline(&line, &llen, tmpl) > 0) {
+		line[llen-1] = '\0';
+		if (*line != delim) {
+			puts(line);
+		} else {
+			putsec(line, src, secmap, size);
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
 	if (argc < 3)
-		usage(argv[0]);
+		goto usage;
 
-	const char delim = '#';
-	const char special = '\0';
+	/* TODO: PARSE OPTS */
+	delim = '#';
+	special = '\0';
 
 	FILE *tmpl = fopen(argv[1], "r");
 	FILE *src  = fopen(argv[2], "r");
 	if (!tmpl || !src)
-		usage(argv[0]);
+		goto usage;
 
-	int nmap = 10;
-	char **smap = calloc(sizeof(*smap), nmap);
-	/*char **smap = NULL;*/
+	unsigned int size = 0;
+	char **secmap = mapsecs(src, &size);
+	if (!secmap)
+		goto errmem;
 
-	char *line = "#@HEADER\n";
-	size_t size = 0;
-	long lno = 0;
-	do {
-		if (lno >= nmap) {
-			nmap *= 2;
-			if (!(smap = realloc(smap, nmap * sizeof(*smap)))) {
-				fprintf(stdout, "realloc fail\n");
-				exit(1);
-			}
-		}
-		if (*line == delim) {
-			smap[lno] = strdup(line);
-		} else {
-			smap[lno] = smap[lno-1];
-		}
-		lno++;
-	} while (getline(&line, &size, src) > 0);
-
-	for (int i = 0; i < nmap; i++)
-		fprintf(stdout, "%d - %s\n", i, smap[i]);
-
-	fprintf(stdout, "exit\n");
+	subst(tmpl, src, secmap, size);
 
 	return EXIT_SUCCESS;
+
+errfile:
+	die("files couldn't be opened");
+	goto usage;
+errmem:
+	die("out of memory");
+	fclose(tmpl);
+	fclose(src);
+usage:
+	die(USAGE);
+	return EXIT_FAILURE;
 }
